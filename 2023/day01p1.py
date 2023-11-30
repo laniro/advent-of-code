ee2b389ec49da45a727a0fb31e67a16fcc6b7ba9
laniro <396sde@gmail.com>
2023-11-30 19:38:44 +0000

    test2

--- 2023/day01p1.py
+++ 2023/day01p1.py
@@ -1,8 +1,8 @@
 for i in range(24):
     for j in range(2):
         if i < 10:
-            fileName = "day0"+i+"p"+j+".py"
+            fileName = "day0"+str(i)+"p"+str(j)+".py"
         else:
-            fileName = "day"+i+"p"+j+".py"
+            fileName = "day"+str(i)+"p"+str(j)+".py"
         with open(fileName,"w") as f:
             pass
