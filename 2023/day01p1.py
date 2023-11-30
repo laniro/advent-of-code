389e31781b90708a901dfd2e2123ae743ee0f040
laniro <396sde@gmail.com>
2023-11-30 19:42:54 +0000

    removing code

--- 2023/day01p1.py
+++
@@ -1,8 +0,0 @@
-for i in range(24):
-    for j in range(2):
-        if i < 10:
-            fileName = "day0"+str(i+1)+"p"+str(j+1)+".py"
-        else:
-            fileName = "day"+str(i+1)+"p"+str(j+1)+".py"
-        with open(fileName,"w") as f:
-            pass
