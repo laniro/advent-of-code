0978f5b0c6c7a2ba9c3a12dba84491cc0ad2a7a7
laniro <396sde@gmail.com>
2023-12-01 15:27:07 +0000

    implemented plans, need to address cases where where multiple of same number. Perhaps change word numbers to digits *before* doing everything else?

--- 2023/day01p2.py
+++ 2023/day01p2.py
@@ -1,25 +1,52 @@
 total = 0
-indices = [1,2,3,4,5,6,7,8,9,"one","two","three","four","five","six","seven","eight","nine"]
-list = []
+indices = ["1","2","3","4","5","6","7","8","9","one","two","three","four","five","six","seven","eight","nine"]
+presentList = []
+indexList = []
 
-with open("2023/test.txt","r") as f:
+def word2num(word):
+    match word:
+        case "one": return 1
+        case "two": return 2
+        case "three": return 3
+        case "four": return 4
+        case "five": return 5
+        case "six": return 6
+        case "seven": return 7
+        case "eight": return 8
+        case "nine": return 9
+        case _: return word
+
+with open("2023/in.txt","r") as f:
     for line in f.readlines():
-        list = []
+        presentList = []
+        indexList = []
         for item in indices:
             if str(item) in line:
-                list.append(item)
+                presentList.append(item)
 
-        # convert words to numbers
-
-        # iterate through presence list and find indices
+        # iterate through presence presentList and find indices
+        for number in presentList:
+            index = line.find(str(number))
+            indexList.append(index)
 
         # generate new list with indices. both lists match ie present numbers [1,2,9] matches to indices [3,0,4]
+        # done in above step
 
-        # get index in presence list of lowest and highest indices ie lowest = 0 --> 2, highest = 4 --> 9
+        # get index in presence presentList of lowest and highest indices ie lowest = 0 --> 2, highest = 4 --> 9
+        lowIndex = presentList[indexList.index(min(indexList))]
+        highIndex = presentList[indexList.index(max(indexList))]
+
+        # convert words to numbers
+        lowIndex = str(word2num(lowIndex))
+        highIndex = str(word2num(highIndex))
 
         # concatenate lowest + highest number
+        finalNum = int(lowIndex+highIndex)
 
         # add to total
+        total+= finalNum
 
-        print(list)
+print(total)
 
+
+# 58ninehxcsnzfxbf6xvgcrfznrldqntsbsjmr5
