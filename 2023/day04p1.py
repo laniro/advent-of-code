38ceb21263078084e03a862ca291635652fd9290
laniro <396sde@gmail.com>
2023-12-04 10:02:39 +0000

    d4p1

---
+++ 2023/day04p1.py
@@ -0,0 +1,16 @@
+import math
+t=0
+with open("2023/in.txt","r") as f:
+    for line in f.readlines():
+        n = 0
+        line = line[line.index(":")+2:].strip()
+        line = line.split(" | ")
+        for item in range(len(line)):
+            line[item] = line[item].split(" ")
+            line[item].sort()
+            for i in range(line[item].count('')):
+                line[item].pop(0)
+        for item in line[0]:
+            if item in line[1]:
+                n += 1
+        t += math.floor(2**(n-1))
