c6e44a39e42c93fa3f0f99c27fd756cc51bababd
laniro <396sde@gmail.com>
2023-12-15 12:43:17 +0000

    working on day12p1

---
+++ 2023/day12p1.py
@@ -0,0 +1,40 @@
+with open("2023/test.txt","r") as f:
+    f = [x.strip() for x in f.readlines()]
+
+def compress(lst):
+    clst = []
+    p = None
+    t=1
+    for c in lst:
+        if p == c:
+            t+=1
+        else:
+            if p != None:
+                clst.append((p*t))
+                t=1
+        p = c
+    clst.append(p*t)
+    return clst
+            
+
+
+for line in f:
+    r,l = line.split()
+    l = [int(x) for x in l.split(",")]
+    r = [x for x in r]
+
+    while True:
+        if r[0] == '.' or r[-1] == '.':
+            if r[0] == '.':
+                r.pop(0)
+            if r[-1] == '.':
+                r.pop(-1)
+        else:
+            break
+        
+    cr = compress(r)
+    
+    if cr[0] == '#'*l[0]:
+        print(cr,"0")
+    if cr[-1] == '#'*l[-1]:
+        print(cr,"-1")
