6b0c63665cab8bac3769caceec75159a172e092e
laniro <396sde@gmail.com>
2024-12-09 11:01:06 +0000

    d6p2, but multiprocessing

--- 2024/d6p2.py
+++ 2024/d6p2.py
@@ -1,10 +1,4 @@
-t=0
-test = "C:/Users/tyler/Documents/advent of code/2024/test.txt"
-inp = "C:/Users/tyler/Documents/advent of code/2024/inp.txt"
-file_path = inp
-with open(file_path, "r") as f:
-    f = f.readlines()
-
+import multiprocessing as mp
 def print_grid(list):
     for line in list:
         print(''.join(line))
@@ -39,61 +33,65 @@ class Guard:
             case "West":
                 this.facing = "North"
 
-f = [list(y) for y in (x.strip() for x in f)]
-
-for r in range(len(f)):
-    for c in range(len(f[r])):
-        if f[c][r] == '^':
-            guard = Guard(c,r,"North")
-
-
-
-grid_size = (len(f)-1, len(f[guard.row])-1)
-og_guard = Guard(guard.column, guard.row, "North")
-
-visited = []
-while (guard.row,guard.column,guard.facing) not in visited:
-    visited.append((guard.row,guard.column,guard.facing))
-    f[guard.row][guard.column] = 'X'
-    guard.moveForward()
-
-    if guard.row < 0 or guard.row > grid_size[0] or guard.column < 0 or guard.column > grid_size[1]: break
-
-    while f[guard.row][guard.column] == '#':
-        guard.moveForward(-1)
-        guard.rotate()
+def run_sim(guard, f, grid_size,cycle = (0,0)):
+    visited = []
+    while (guard.row,guard.column,guard.facing) not in visited:
+        visited.append((guard.row,guard.column,guard.facing))
+        f[guard.row][guard.column] = 'X'
         guard.moveForward()
+        if guard.row < 0 or guard.row > grid_size[0] or guard.column < 0 or guard.column > grid_size[1]: 
+            return visited
 
-just_positions = []
-for x in visited:
-    just_positions.append((x[0],x[1]))
-
-for i in range(grid_size[0]+1):
-    for j in range(grid_size[1]+1):
-        if (i,j) not in just_positions: continue
-        print(i,j)
-        # reset everything
-        guard = Guard(og_guard.column, og_guard.row, "North")
-        with open(file_path, "r") as f:
-            f = f.readlines()
-        f = [list(y) for y in (x.strip() for x in f)]
-        f[i][j] = '#'
-        
-        # main loop
-        visited = []
-        while (guard.row,guard.column,guard.facing) not in visited:
-            visited.append((guard.row,guard.column,guard.facing))
-            f[guard.row][guard.column] = 'X'
+        while f[guard.row][guard.column] == '#':
+            guard.moveForward(-1)
+            guard.rotate()
             guard.moveForward()
+    print(cycle)
+    return True
 
-            if guard.row < 0 or guard.row > grid_size[0] or guard.column < 0 or guard.column > grid_size[1]: break
 
-            while f[guard.row][guard.column] == '#':
-                guard.moveForward(-1)
-                guard.rotate()
-                guard.moveForward()
-        else:
+if __name__ == "__main__":
+    t=0
+    test = "C:/Users/tyler/Documents/advent of code/2024/test.txt"
+    inp = "C:/Users/tyler/Documents/advent of code/2024/inp.txt"
+    file_path = inp
+    with open(file_path, "r") as f:
+        f = f.readlines()
+
+    f = [list(y) for y in (x.strip() for x in f)]
+
+    for r in range(len(f)):
+        for c in range(len(f[r])):
+            if f[c][r] == '^':
+                guard = Guard(c,r,"North")
+
+    grid_size = (len(f)-1, len(f[guard.row])-1)
+    og_guard = Guard(guard.column, guard.row, "North")
+
+    just_positions = []
+    visited = run_sim(guard,f,grid_size)
+    for x in visited:
+        just_positions.append((x[0],x[1]))
+    
+    p = mp.Pool()
+    args = []
+    for i in range(grid_size[0]+1):
+        for j in range(grid_size[1]+1):
+            if (i,j) not in just_positions: continue
+            # reset everything
+            guard = Guard(og_guard.column, og_guard.row, "North")
+            with open(file_path, "r") as f:
+                f = f.readlines()
+            f = [list(y) for y in (x.strip() for x in f)]
+            f[i][j] = '#'
+            
+            # main loop
+            args.append([guard,f,grid_size,(i,j)])
+
+    res = p.starmap(run_sim,args)
+
+    for r in res:
+        if r == True:
             t+=1
 
-print(t)
-running = False
+    print(t)
