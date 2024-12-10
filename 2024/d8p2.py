c24605248f6fc752b299ed07aa14af976a69033b
laniro <396sde@gmail.com>
2024-12-10 15:02:45 +0000

    d8p2

---
+++ 2024/d8p2.py
@@ -0,0 +1,31 @@
+from aoclib import Vector2
+
+if __name__ == "__main__":
+    test = "C:/Users/tyler/Documents/advent of code/2024/test.txt"
+    inp = "C:/Users/tyler/Documents/advent of code/2024/inp.txt"
+    file_path = inp
+    with open(file_path, "r") as f:
+        f = f.readlines()
+    f = [list(y) for y in (x.strip() for x in f)]
+    locations = {}
+    for r in range(len(f)):
+        for c in range(len(f[r])):
+            if f[r][c] != '.':
+                if f[r][c] in locations.keys():
+                    locations[f[r][c]].append(Vector2(r,c))
+                else:
+                    locations.update({f[r][c]:[Vector2(r,c)]})
+
+    antennae = []
+    for antenna in locations:
+        for p1 in locations[antenna]:
+            for p2 in locations[antenna]:
+                if p1 == p2: continue
+                print(p1,p2)
+                vec = p2 - p1
+                for i in range(len(f)):
+                    pos = p1 + i * vec
+                    if pos.x < 0 or pos.x >= len(f[0]) or pos.y < 0 or pos.y >= len(f) or pos in antennae: continue
+                    antennae.append(pos)
+
+    print(len(antennae))
