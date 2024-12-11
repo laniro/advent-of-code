d599a9eedc471a9f420af514b0b9827a7b0d4c2d
laniro <396sde@gmail.com>
2024-12-11 09:39:29 +0000

    d9p2

--- 2024/d9p2.py
+++ 2024/d9p2.py
@@ -4,13 +4,13 @@ if __name__ == "__main__":
     start = timer()
     test = "C:/Users/tyler/Documents/advent of code/2024/test.txt"
     inp = "C:/Users/tyler/Documents/advent of code/2024/inp.txt"
-    file_path = test
+    file_path = inp
     with open(file_path, "r") as f:
         f = f.readlines()
     f = [list(y) for y in f][0]
 
     free = []
-    main_list = []
+    disk = []
     mode = 'file'
     id = 0
     files = {}
@@ -22,69 +22,69 @@ if __name__ == "__main__":
             case 'file':
                 mode = 'free'
                 for i in range(x):
-                    main_list.append(str(id))
+                    disk.append(str(id))
                 files.update({id: x})
                 id += 1
             case 'free':
                 mode = 'file'
                 if x > 0:
-                    main_list.append('.' * x)
+                    disk.append('.' * x)
     print(f"Initialising drive... 100% complete.")
     print(f"Took {timer() - start} seconds.")
 
     frees = {}
-    starting_length = len(main_list)
-    for i in range(len(main_list)):
+    starting_length = len(disk)
+    for i in range(len(disk)):
         print(f"Reorganising array... {int(i/starting_length*100)}% complete")
-        if '.' not in main_list[i]: continue
+        if '.' not in disk[i]: continue
         if i in frees.keys(): continue
         
-        length = len(main_list[i])
+        length = len(disk[i])
         for j in range(length):
-            frees.update({i+j:length})
-        main_list.pop(i)
+            frees.update({i+j:length-j})
+        disk.pop(i)
 
         for x in range(length):
-            main_list.insert(i, '.')
+            disk.insert(i, '.')
     print(f"Reorganising array... 100% complete")
     print(f"Took {timer() - start} seconds.")
 
     for key in list(files.keys())[::-1]:
+        ind = disk.index(str(key))
+        if len(frees.values()) == 0: 
+            print("No free space on disk. Breaking")
+            break
         if files[key] > max(frees.values()): 
             print(f"File {key} larger than all free space on disk. Skipping")
             continue
-        for i in range(main_list.index('.'),len(main_list)):
-            if '.' not in main_list[i]: 
+        
+        for i in range(disk.index('.'),len(disk)):
+            if i > ind: 
+                print(f"Trying to move file {key} right. Skipping")
+                break
+            if '.' not in disk[i]: 
                 #print("No '.' at ", i,", Continuing")
                 continue
-            if i not in frees.keys(): 
-                #print("No free space at index ",i)
-                continue
-            if files[key] > frees[i]: 
+            if i not in frees.keys() or files[key] > frees[i]: 
                 #print("File bigger than free space at index ", i)
                 continue
 
             print(f"File {key} moved to index {i}. File of length {files[key]}, free space of length {frees[i]}")
             
-            ind = main_list.index(str(key))
-            for j in range(10):
-                if ind+j >= len(main_list): break
-                if main_list[ind + j] == str(key):
-                    main_list[ind+j] = '.'
-            
             for j in range(files[key]):
-                main_list[i+j] = str(key)
-                frees[i+j] = 0
-                if i+j+1 not in frees.keys(): break
-                frees[i+j+1] -= files[key]
+                if disk[ind + j] == str(key):
+                    disk[ind+j] = '.'
+                    frees.update({ind+j:files[key]-j})
+                disk[i+j] = str(key)
+                frees.pop(i+j)
             break
         else:
             print(f"No space was found for file {key} of length {files[key]}")
 
     checksum = 0
     
-    for i in range(len(main_list)):
-        if '.' in main_list[i]: continue
-        checksum += i * int(main_list[i])
+    for i in range(len(disk)):
+        if '.' in disk[i]: continue
+        checksum += i * int(disk[i])
     print(checksum)
     print(f"Took {timer() - start} seconds.")
